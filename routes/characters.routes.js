eedbf61138804740e1ce49b918976827fff8829a
kirran01 <Kirran-@live.com>
2022-11-17 14:35:29 -0500

    add a character

--- routes/characters.routes.js
+++ routes/characters.routes.js
@@ -2,26 +2,55 @@ const router = require("express").Router();
 const axios = require("axios");
 
 /* GET home page */
-router.get("/characters", (req, res, next) => {
-    axios.get("https://ih-crud-api.herokuapp.com/characters")
-    .then(responseFromAPI => {
-        // console.log(responseFromAPI)
-        res.render("characters/list-characters", { characters: responseFromAPI.data });
-    })
-    .catch(err => console.error(err))
+
+router.get("/characters/create", (req, res) => {
+  res.render("characters/create-character.hbs");
 });
 
+router.get("/characters", (req, res, next) => {
+  axios
+    .get("https://ih-crud-api.herokuapp.com/characters")
+    .then((responseFromAPI) => {
+      // console.log(responseFromAPI)
+      res.render("characters/list-characters", {
+        characters: responseFromAPI.data,
+      });
+    })
+    .catch((err) => console.error(err));
+});
 
 router.get("/characters/:id", (req, res, next) => {
-    axios.get(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
-    .then(responseFromAPI => {
-        // console.log("details: ", responseFromAPI.data)
-        res.render("characters/details-character", { character: responseFromAPI.data });
+  axios
+    .get(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
+    .then((responseFromAPI) => {
+      // console.log("details: ", responseFromAPI.data)
+      res.render("characters/details-character", {
+        character: responseFromAPI.data,
+      });
     })
-    .catch(err => console.error(err))
+    .catch((err) => console.error(err));
 });
 
-module.exports = router;
+router.post("/characters/create", (req, res) => {
+  //   if (req.body.debt === "true") {
+  //     req.body.debt = true;
+  //   } else {
+  //     req.body.debt = false;
+  //   }
+  axios
+    //   in this instance, req.body is already the object with appropriate key val pairs
+    //   so we can just send the entire req.body into axios.post and it will insert it.
+    .post("https://ih-crud-api.herokuapp.com/characters", req.body)
+    .then(() => {
+      res.redirect("/characters");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
+
+
+module.exports = router;
 
 // https://ih-crud-api.herokuapp.com/characters
--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const app = require("./app");
 
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 3000
 const PORT = process.env.PORT || 3000;
+let d = new Date();
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port http://localhost:${PORT}`);
+  console.log(
+    `Server listening on port http://localhost:${PORT} time: ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`
+  );
 });
